b6574a359d5f5ef9041ed1b16f4b9fa17fc451ab
jonathan-annett <jonathan.max.annett@gmail.com>
2020-02-17 17:23:26 +1100

    updated to reflect new module format

--- node-js-demo.js
+++ node-js-demo.js
@@ -3,7 +3,7 @@ try {
     express=require("express"),
     app=express();
     
-    require("./index.js").express(express,app);
+    require("./index.js")().express(express,app);
     
     require('get-localhost-hostname/start-browser.js')(app,0,"/perf_now_time");
     
